df111b7bcdcdbfa326fb4513e0e935d0777ceeed
Rippingcorn21 <pbegkas98@outlook.com>
2017-11-26 20:22:25 +0200

    Filecretor clean up

--- src/textGenerator/FileCreator.java
+++ src/textGenerator/FileCreator.java
@@ -1,12 +1,18 @@
 package textGenerator;
 
+import java.util.ArrayList;
+
+import dataModel.Movie;
+
 public abstract class FileCreator {
 	private ArrayList<Movie> input;
 	private String path;
 	public String produceText() {
+		return "0";
 		
 	}
 	public String createFile() {
+		return "0";
 		
 	}
 }
