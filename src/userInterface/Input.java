76b8e4e9bf6785e57b935a7857e2d283bfc0397e
Phil <philpapachristou@gmail.com>
2017-11-26 20:47:34 +0200

    minor correction in input.java

--- src/userInterface/Input.java
+++ src/userInterface/Input.java
@@ -2,7 +2,6 @@ package userInterface;
 import textGenerator.TextCreator;
 import dataSearch.DataFinder;
 import dataSearch.DataRegister;
-import dataSearch.DataCalculator;
 //User interaction
 public class Input {
 
